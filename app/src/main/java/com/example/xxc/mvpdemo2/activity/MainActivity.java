d967f52e6d932c2e9c97a442c98e878bc3f99c16
xxc-xxc <55835158xxc-xxc@users.noreply.github.com>
2020-05-05 23:33:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xxc/mvpdemo2/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.xxc.mvpdemo2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.xxc.mvpdemo2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
